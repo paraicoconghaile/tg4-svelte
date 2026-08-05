370d3635e3da1b19ca6e1d6d987254c75dc9662d
Paraic Ó Conghaile <congha-p@tg4.local>
2026-08-05 15:19:49 +0100

    Configure SvelteKit for Amplify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
-		sveltekit({
+		/* sveltekit({
 			compilerOptions: {
 				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 				runes: ({ filename }) =>
@@ -14,7 +14,8 @@ export default defineConfig({
 			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-			adapter: adapter()
-		})
+			//adapter: adapter()
+		}) */
+        sveltekit()
 	]
 });
